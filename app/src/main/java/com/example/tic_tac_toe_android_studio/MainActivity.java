ae2a1c6bcd55efe6a52d40d429a5ad0c40c10335
salemalem <wymedia@protonmail.com>
2019-11-12 19:05:43 +0600

    Now if you click counter it will not be rewritten as previous version.
    Game has state. So if you place somewhere red. You can't touch there and place yellow, or red again. But you can use free, untouched space.

--- app/src/main/java/com/example/tic_tac_toe_android_studio/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe_android_studio/MainActivity.java
@@ -12,20 +12,30 @@ public class MainActivity extends AppCompatActivity {
     // 0 = yellow, 1 = red
     int activePlayer = 0;
 
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    // 2 means unplayed
+
     public void dropIn(View view){
         ImageView counter = (ImageView) view;
+        //  System.out.println(counter.getTag().toString());
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setTranslationY(-1000f);
+        if (gameState[tappedCounter] == 2) {
+            gameState[tappedCounter] = activePlayer;
 
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
-        } else {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1000f).rotation(1000).setDuration(900);
+
         }
-
-        counter.animate().translationYBy(1000f).rotation(1000).setDuration(900);
     }
 
     @Override
